e4a91c4b1eec35332d0d90a0ae77677abd5a2326
Flavio Silva <flavioms86@hotmail.com>
2021-05-18 15:38:39 -0300

    Melhorias na New Register Activity

--- app/src/main/java/com/example/minhapressaoarterial/NewRegisterActivity.kt
+++ app/src/main/java/com/example/minhapressaoarterial/NewRegisterActivity.kt
@@ -2,10 +2,33 @@ package com.example.minhapressaoarterial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class NewRegisterActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var textTitle: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_register)
+
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        textTitle = findViewById(R.id.toolbar_title)
+        textTitle.text = "NOVO REGISTRO"
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
